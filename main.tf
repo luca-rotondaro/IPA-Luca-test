f6e12f6d9ee405df758f517871f6d3491ce9b664
luca-rotondaro <98099333+luca-rotondaro@users.noreply.github.com>
2022-03-23 13:46:44 +0100

    variable changed

--- main.tf
+++ main.tf
@@ -34,51 +34,51 @@ locals {
 
 # Create a resource group rg-"cusname_short"-chn-management
 resource "azurerm_resource_group" "management" {
-  name     = "rg-${local.local_data.result.customer.custCustomerNameShort}-${var.azregion}-${var.management}"
+  name     = "rg-${local.local_data.result.customer.shortName}-${var.azregion}-${var.management}"
   location = var.location
 
   tags = {
     environment = var.services
-    owner       = local.local_data.result.customer.custCustomerNameShort
-    creator     = local.local_data.result.customer.custCustomerNameShort
+    owner       = local.local_data.result.customer.shortName
+    creator     = local.local_data.result.customer.shortName
   }
 }
 
 # Create a resource group rg-"cusname_short"-chn-services
 resource "azurerm_resource_group" "services" {
-  name     = "rg-${local.local_data.result.customer.custCustomerNameShort}-${var.azregion}-${var.services}"
+  name     = "rg-${local.local_data.result.customer.shortName}-${var.azregion}-${var.services}"
   location = var.location
 
   tags = {
     environment = var.services
-    owner       = local.local_data.result.customer.custCustomerNameShort
-    creator     = local.local_data.result.customer.custCustomerNameShort
+    owner       = local.local_data.result.customer.shortName
+    creator     = local.local_data.result.customer.shortName
   }
 }
 
 # Create a resource group rg-"cusname_short"-chn-connectivity
 resource "azurerm_resource_group" "connectivity" {
-  name     = "rg-${local.local_data.result.customer.custCustomerNameShort}-${var.azregion}-${var.connectivity}"
+  name     = "rg-${local.local_data.result.customer.shortName}-${var.azregion}-${var.connectivity}"
   location = var.location
 
   tags = {
     environment = var.services
-    owner       = local.local_data.result.customer.custCustomerNameShort
-    creator     = local.local_data.result.customer.custCustomerNameShort
+    owner       = local.local_data.result.customer.shortName
+    creator     = local.local_data.result.customer.shortName
   }
 }
 
 # Create virtual network (vnet)
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-${local.local_data.result.customer.custCustomerNameShort}-${var.azregion}-${var.connectivity}"
+  name                = "vnet-${local.local_data.result.customer.shortName}-${var.azregion}-${var.connectivity}"
   address_space       = var.address_space
   location            = var.location
   resource_group_name = azurerm_resource_group.connectivity.name
 
   tags = {
     environment = var.services
-    owner       = local.local_data.result.customer.custCustomerNameShort
-    creator     = local.local_data.result.customer.custCustomerNameShort
+    owner       = local.local_data.result.customer.shortName
+    creator     = local.local_data.result.customer.shortName
   }
 }
 
@@ -116,13 +116,13 @@ resource "azuread_group" "current" {
 
 resource "azuread_user" "current" {
   user_principal_name = local.local_data.result.snow.snowChangeRequester
-  display_name        = local.local_data.result.customer.custCustomerNameFull
-  mail_nickname       = "${local.local_data.result.customer.custCustomerNameShort}-${var.azregion}"
+  display_name        = local.local_data.result.customer.fullName
+  mail_nickname       = "${local.local_data.result.customer.shortName}-${var.azregion}"
   password            = "SecretP@sswd99!"
 }
 
 resource "azurerm_role_definition" "role" { #create roledefinition ad-role-rot-mgmt
-  name  = "ad-role-${local.local_data.result.customer.custCustomerNameShort}-${var.management}"
+  name  = "ad-role-${local.local_data.result.customer.shortName}-${var.management}"
   scope = local.local_data.result.azure.subscription_id
 
   permissions {
@@ -144,13 +144,13 @@ resource "azurerm_role_assignment" "current" {
 #Cost Management and Notifications
 
 resource "azurerm_monitor_action_group" "management" {
-  name                = "sub-budget-${local.local_data.result.customer.custCustomerNameShort}"
+  name                = "sub-budget-${local.local_data.result.customer.shortName}"
   resource_group_name = azurerm_resource_group.management.name
-  short_name          = "agroup-${local.local_data.result.customer.custCustomerNameShort}"
+  short_name          = "agroup-${local.local_data.result.customer.shortName}"
 }
 
 resource "azurerm_consumption_budget_resource_group" "management" {
-  name              = "sub-budget-${local.local_data.result.customer.custCustomerNameShort}"
+  name              = "sub-budget-${local.local_data.result.customer.shortName}"
   resource_group_id = azurerm_resource_group.management.id
 
   amount     = local.local_data.result.customer.budget
@@ -163,9 +163,9 @@ resource "azurerm_consumption_budget_resource_group" "management" {
 
   filter {
     tag {
-      name = "Monitoring-${local.local_data.result.customer.custCustomerNameShort}"
+      name = "Monitoring-${local.local_data.result.customer.shortName}"
       values = [
-        local.local_data.result.customer.custCustomerNameShort,
+        local.local_data.result.customer.shortName,
       ]
     }
   }
